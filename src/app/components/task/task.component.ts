7e309a709bd88cf8915f931828165f56d37412b6
ELOI PARDO RIVERO <epardo@futurshealth.com>
2020-12-01 08:27:19 +0100

    add onpush to task

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ChangeDetectionStrategy } from "@angular/core";
+
 import { ITask } from "src/app/interfaces/task";
 
 @Component({
     selector: "app-task",
     templateUrl: "./task.component.html",
     styleUrls: ["./task.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskComponent implements OnInit {
     @Input() task: ITask;
